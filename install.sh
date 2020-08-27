966f88fa09997d7cd46a14423a21124ef806d1f8
Justin Pietsch <jpietsch@gmail.com>
2020-08-27 15:21:56 -0700

    prometheus and telegraf work. need to figure out grafana

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 # I don't know if an install script is best, but I'll start here
 
 # need to figure out a more stable URL for this
+
+# apt-get install snmp-mibs-downloader
+# https://hub.docker.com/r/nuntz/telegraf-snmp/dockerfile
 mkdir mibs
 cd mibs
 wget https://www.juniper.net/documentation/software/junos/junos194/juniper-mibs-19.4R2.6.tgz -O - | tar xz
