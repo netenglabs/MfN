6ef8944491aa99a0a169715dbab460af46147ec1
Justin Pietsch <jpietsch@gmail.com>
2020-09-04 13:26:01 -0700

    a little more progress, some new juniper mibs and set grafana to default prometheus datasource

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@
 
 # apt-get install snmp-mibs-downloader
 # https://hub.docker.com/r/nuntz/telegraf-snmp/dockerfile
+# https://www.homelabrat.com/snmp-and-telegraf/
 mkdir mibs
 cd mibs
 wget https://www.juniper.net/documentation/software/junos/junos194/juniper-mibs-19.4R2.6.tgz -O - | tar xz
